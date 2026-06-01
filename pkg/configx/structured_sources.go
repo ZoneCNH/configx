b4bae7a7607d7fe4091b9ee4f714e766d72dc9f9
黄博 <112565441+ZoneCNH@users.noreply.github.com>
2026-06-01 13:34:48 +0800

    wip(config): add structured source skeleton

--- pkg/configx/structured_sources.go
+++ pkg/configx/structured_sources.go
@@ -1,1 +1,7 @@
 package configx
+
+import "context"
+
+func LoadTOMLFile(ctx context.Context, path string) (LoadResult, error) {
+	return NewLoader().AddSource(NewMapSource("toml", map[string]string{})).Load(ctx)
+}
